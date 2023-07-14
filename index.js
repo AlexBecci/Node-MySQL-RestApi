05ccf0a0b05ba218474875400607fe3449bd2dd1
AlexBecci <beccialex2002@gmail.com>
2023-07-14 12:46:14 -0300

    Endpoints

--- index.js
+++ index.js
@@ -2,6 +2,22 @@ import express from "express";
 
 const app = express();
 
-app.listen(6000)
+app.get("/employees", (req, res) => {
+  res.send("obteniendo empleados");
+});
 
-console.log('Server runnin on port 3000')
+app.post("/employees", (req, res) => {
+  res.send("creando empleados");
+});
+
+app.put("/employees", (req, res) => {
+  res.send("actualizando empleados");
+});
+
+app.delete("/employees", (req, res) => {
+  res.send("eliminando empleados");
+});
+
+app.listen(3000);
+
+console.log("Server runnin on port 3000");
